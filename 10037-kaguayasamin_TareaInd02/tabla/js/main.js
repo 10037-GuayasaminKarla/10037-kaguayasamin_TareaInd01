0a4de19399532ca0c96417ac0af4d94489b05f1a
Karla Andrea Guayasamin Diaz <107556820+10037-GuayasaminKarla@users.noreply.github.com>
2023-05-25 11:34:11 -0500

    Delete 10037-kaguayasamin_TareaInd02 directory

--- 10037-kaguayasamin_TareaInd02/tabla/js/main.js
+++
@@ -1,7 +0,0 @@
-function cambiarEstilo() {
-  var titulo = document.getElementById("titulo");
-
-  // Cambiar el color del h1 agregando la clase 'color-change'
-  titulo.classList.toggle("color-change");
-  titulo.classList.toggle("align-text");
-}
